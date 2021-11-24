8a6960fe920c7d020eb407268f97dd154750c9d9
AmitSahu27081999 <amitsahu27081999@gmail.com>
2021-11-24 17:29:29 +0530

    second time UI

--- application/views/customer/admin/shared/customer_css.php
+++ application/views/customer/admin/shared/customer_css.php
@@ -1,26 +1,26 @@
 <!-- Animate.css -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/animate.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/animate.css");?>">
 <!-- Icomoon Icon Fonts-->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/icomoon.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/icomoon.css");?>">
 <!-- Ion Icon Fonts-->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/ionicons.min.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/ionicons.min.css");?>">
 <!-- Bootstrap  -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/bootstrap.min.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/bootstrap.min.css");?>">
 
 <!-- Magnific Popup -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/magnific-popup.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/magnific-popup.css");?>">
 
 <!-- Flexslider  -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/flexslider.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/flexslider.css");?>">
 
 <!-- Owl Carousel -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/owl.carousel.min.css");?>">
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/owl.theme.default.min.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/owl.carousel.min.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/owl.theme.default.min.css");?>">
 	
 <!-- Date Picker -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/bootstrap-datepicker.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/bootstrap-datepicker.css");?>">
 <!-- Flaticons  -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/fonts/flaticon.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/fonts/flaticon.css");?>">
 
 <!-- Theme style  -->
-<link rel="stylesheet" href="<?= base_url("/assets/admin/css/style.css");?>">
+<link rel="stylesheet" href="<?= base_url("/assets/app/css/style.css");?>">
